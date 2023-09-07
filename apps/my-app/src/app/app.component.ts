342d26ff9ef844dfa1bb27df8b95fd53c8e915a7
tamyfabius <tamyfabius@gmail.com>
2023-09-07 13:25:25 +0200

    1 file modified

--- apps/my-app/src/app/app.component.ts
+++ apps/my-app/src/app/app.component.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
 
-getDofTicketsStatics(): Observable<DofTicketsStatisticsModel[]> {
-  return this._dofService
-    .getPageableListWithQueryParams<DofTicketsStatisticsModel>(
-      '/dof-ticket-statistics/'
-    )
-    .pipe(
-      map((res: PageableModel<DofTicketsStatisticsModel>) => res.results)
-    );
-}
-
-export interface PageableModel<T> {
-  previous?: string;
-  next?: string;
-  count: number;
-  results: T[];
-}
-
-export interface DofTicketsStatisticsModel {
-  nb_opened_tickets: number;
-  nb_self_assigned_tickets: number;
-  nb_low_priority_tickets: number;
-  nb_medium_priority_tickets: number;
-  nb_high_priority_tickets: number;
-  nb_tickets_in_validation_status: number;
-  nb_tickets_in_waiting_update_status: number;
-  nb_tickets_in_implementation_status: number;
-  nb_tickets_in_testing_status: number;
+@Component({
+  selector: 'stackblitz-nx-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'my-app';
 }
